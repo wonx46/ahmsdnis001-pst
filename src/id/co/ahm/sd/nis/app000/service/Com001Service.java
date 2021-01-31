a40be67b575a49d721eaa1d1db771bab62ae18f0
Iwan abdur rakhman <wonx.project@gmail.com>
2021-01-31 12:30:38 +0700

    initial commit

---
+++ src/id/co/ahm/sd/nis/app000/service/Com001Service.java
@@ -0,0 +1,19 @@
+package id.co.ahm.sd.nis.app000.service;
+
+import java.util.List;
+
+import id.co.ahm.sd.nis.app000.model.AhmsdnisMstbrnd;
+
+public interface Com001Service {
+	
+
+	public void addMstBrand(AhmsdnisMstbrnd brand);
+
+	public List<AhmsdnisMstbrnd> getAllBrand();
+
+	public void deleteBrand(String brandId);
+
+	public AhmsdnisMstbrnd updateBrand(AhmsdnisMstbrnd brand);
+
+	public AhmsdnisMstbrnd getBrand(String brandId);
+}
