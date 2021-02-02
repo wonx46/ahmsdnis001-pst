952b92e497c9d9f43b19cf54c359a5239a1e0754
Iwan abdur rakhman <wonx.project@gmail.com>
2021-02-02 14:40:18 +0700

    integrasi ahm

---
+++ src/id/co/ahm/sd/nis/app000/config/DbConnection.java
@@ -0,0 +1,38 @@
+package id.co.ahm.sd.nis.app000.config;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+//@PropertySource("classpath:database.properties")
+public class DbConnection {
+
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    @Value("${jdbc.driver}")
+    private String driver;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        try {
+            dataSource.setPassword(password);
+        } catch (Exception ex) {
+            Logger.getLogger(DbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dataSource;
+    }
+}
--- src/id/co/ahm/sd/nis/app000/rest/Com001Controller.java
+++ src/id/co/ahm/sd/nis/app000/rest/Com001Controller.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
